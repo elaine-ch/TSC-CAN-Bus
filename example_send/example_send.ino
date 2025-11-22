86dacd6f915991286cd7c82011a0c5de0bdffc9c
Aarnav Gujjari <aarnavg@Aarnavs-MacBook-Air.local>
2025-11-22 14:16:31 -0800

    Able to send HELLO between two nodes

--- example_send/example_send.ino
+++ example_send/example_send.ino
@@ -3,6 +3,7 @@
 
 
 #include <SPI.h>
+#define SERIAL_PORT_MONITOR Serial
 
 #define CAN_2515
 //#define CAN_2518FD
@@ -38,32 +39,22 @@ mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
 
 void setup() {
     SERIAL_PORT_MONITOR.begin(115200);
-    while(!Serial){};
+    while(!SERIAL_PORT_MONITOR){};
 
     while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
         SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
         delay(100);
     }
-    SERIAL_PORT_MONITOR.println("CAN init ok!");
+    SERIAL_PORT_MONITOR.println("CAN init ok! Sending 'HELLO!!!'");
 }
 
-unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char stmp[8] = {'H', 'E', 'L', 'L', 'O', '!', '!', '!'};
 void loop() {
     // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
-    stmp[7] = stmp[7] + 1;
-    if (stmp[7] == 100) {
-        stmp[7] = 0;
-        stmp[6] = stmp[6] + 1;
-
-        if (stmp[6] == 100) {
-            stmp[6] = 0;
-            stmp[5] = stmp[5] + 1;
-        }
-    }
 
     CAN.sendMsgBuf(0x00, 0, 8, stmp);
-    delay(100);                       // send data per 100ms
-    SERIAL_PORT_MONITOR.println("CAN BUS sendMsgBuf ok!");
+    SERIAL_PORT_MONITOR.println("CAN BUS Sent: HELLO!!!");
+    delay(1000); // Send data every 1000ms
 }
 
 // END FILE
